e45507ab15abe2b0973b4066f1297f209dd9427d
Marc Würth <ravage@bluewin.ch>
2014-10-24 01:42:42 +0200

    Added the file header doc block

--- bin/cs-drive-backuper.bat
+++ bin/cs-drive-backuper.bat
@@ -1,7 +1,18 @@
+::
+:: CloudSigma Drive Backuper
+::
+:: Licensed under the MIT license.
+:: For full copyright and license information, please see the LICENSE.
+::
+:: @copyright ORCA Services AG
+:: @link https://github.com/orca-services/cloudsigma-drive-backuper
+:: @license http://www.opensource.org/licenses/mit-license.php MIT License
+:: @author ORCA Services AG <development@orca-services.ch>
+::
+
 @echo.
 @echo off
 echo %~dp0
 php -q "%~dp0\cs-drive-backuper" %*
-REM -working "%CD% " %*
 echo.
 exit /B %ERRORLEVEL%
--- src/CloudSigmaDriveBackuper.php
+++ src/CloudSigmaDriveBackuper.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * CloudSigma Drive Backuper
+ *
+ * Licensed under the MIT license.
+ * For full copyright and license information, please see the LICENSE.
+ *
+ * @copyright ORCA Services AG
+ * @link https://github.com/orca-services/cloudsigma-drive-backuper
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @author ORCA Services AG <development@orca-services.ch>
+ */
 
 namespace OrcaServices\CloudSigmaDriveBackuper;
 
